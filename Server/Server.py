537c7a31ec21345eefe5a42692cc8c79803434f6
IanMSIshikawa <ianishikawa1@gmail.com>
2024-06-22 17:48:01 -0300

    realizando critografia e descriptografia

--- Server/Server.py
+++ Server/Server.py
@@ -1,38 +1,61 @@
 import socket
+from cryptography.fernet import Fernet
+
+def binary_to_string(binary_input):
+    # Verifica se o comprimento do input binário é múltiplo de 8
+    if len(binary_input) % 8 != 0:
+        raise ValueError("O comprimento da entrada binária deve ser múltiplo de 8.")
+    
+    # Lista para armazenar os caracteres convertidos
+    chars = []
+
+    # Divide a entrada binária em blocos de 8 bits e converte cada bloco
+    for i in range(0, len(binary_input), 8):
+        byte = binary_input[i:i+8]
+        # Converte o bloco de 8 bits em um valor decimal
+        decimal = int(byte, 2)
+        # Converte o valor decimal em um caractere ASCII e adiciona à lista
+        chars.append(chr(decimal))
+    
+    # Junta os caracteres em uma string e retorna
+    return ''.join(chars)
 
 
 def start_server(ip, port):
+
     # Cria um socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Vincula o socket ao endereço IP e porta especificados
     server_socket.bind((ip, port))
-
-    # Coloca o socket em modo de escuta
     server_socket.listen(5)
     print(f"Servidor escutando em {ip}:{port}")
+
+    #abre a chave de criptografia
+    with open('chave.key', 'rb') as filekey:
+        chave = filekey.read()
+    fernet = Fernet(chave)
 
     while True:
         # Aceita uma conexão de um cliente
         client_socket, client_address = server_socket.accept()
-        print(f"Conexão aceita de {client_address}")
+        print(f"Conexão aceita de {client_address}\n")
 
         while True:
             # Recebe mensagem do cliente
-            message = client_socket.recv(1024).decode('utf-8')
-            if not message:
-                # Se a mensagem estiver vazia, o cliente fechou a conexão
-                print("Conexão encerrada pelo cliente")
-                break
+            encrypeted_message = client_socket.recv(1024).decode('utf-8')
+            decrypt_message = fernet.decrypt(encrypeted_message).decode('utf-8')
+            message = binary_to_string(decrypt_message)
+            
 
-            # Imprime a mensagem recebida
-            print(f"Mensagem recebida: {message}")
+            print(f"Mensagem recebida criptografa: {encrypeted_message}\n")
+            print(f"Mensagem recebida descriptografa: {decrypt_message}\n")
+            print(f"Mensagem recebida: {message}\n")
 
-        # Fecha a conexão com o cliente
+
+
         client_socket.close()
 
 if __name__ == "__main__":
-    # Especifica o endereço IP e a porta
+
     IP = "127.0.0.1"
     PORT = 8080
 
