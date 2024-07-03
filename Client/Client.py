ba8fafaaac04509428d9cb1cbbaded6617d4ff1e
IanMSIshikawa <ianishikawa1@gmail.com>
2024-07-02 22:54:17 -0300

    finalizado

--- Client/Client.py
+++ Client/Client.py
@@ -23,82 +23,41 @@ def start_client(ip, port):
         print(f"Conectado ao servidor em {ip}:{port}\n")
         try:
             while True:
-                # #Input de entrada
-                # message = input(f"Digite a mensagem para enviar ao servidor (ou 'exit' para sair):\n ")
-                # #Processamentos da mensagem
-                # crypto_message = fernet.encrypt(message.encode('UTF-8'))
-                # binary_message = Alg.string_to_binary(crypto_message)
-                # message_6t = Alg.binary_to_ternary(binary_message, map8b6t)
-                # #Saída dos processamentos
-                # print(f"Mensagem enviada: {message}\n")
-                # print(f"Mensagem enviada criptografada: {crypto_message}\n")
-                # print(f"Mensagem enviada em binário: {binary_message}\n")
-                # print(f"Mensagem enviada em ternário: {message_6t}\n")
-
-                # Alg.plot_signal(message_6t)
-
-                # #Envio da mensagem
-                # connection.send_message(message_6t.encode())
-
-            #-----------------------------------------SEM CRIPTOGRAFIA------------------------------------------------
-
-
                 #Input de entrada
                 message = input(f"Digite a mensagem para enviar ao servidor (ou 'exit' para sair):\n ")
                 #Processamentos da mensagem
-                binary_message = message
-
-                message_6t = pattern_to_pattern(binary_message)
-
-                #balanço DC
-                positive = message_6t.count('+')
-                negative = message_6t.count('-')
-                saldo = positive - negative
-                tamanho_bloco = 6
-                if ((saldo) >= 2 or (saldo) <= -2):
-                    j = 0
-                    balanco = 0
-                    for i in range(0, len(message_6t), tamanho_bloco):
-                        bloco = message_6t[i:i + tamanho_bloco]
-                        quantidade_mais = bloco.count('+')
-                        quantidade_menos = bloco.count('-')
-                        balanco[j] = quantidade_mais - quantidade_menos
-                        j = j+1
-                    j = j-1
-                    while (saldo >= 2):
-                        if(balanco[j] == 1):
-                            for i in range(j*6, j*6+6, 1):
-                                if (message_6t[i] == '+'):
-                                    message_6t[i] = '-'
-                                elif (message_6t[i] == '-'):
-                                    message_6t[i] = '+'
-                            saldo =  saldo - 2
-                        j = j-1
-                    while (saldo <= -2):
-                        if(balanco[j] == -1):
-                            for i in range(j*6, j*6+6, 1):
-                                if (message_6t[i] == '+'):
-                                    message_6t[i] = '-'
-                                elif (message_6t[i] == '-'):
-                                    message_6t[i] = '+'
-                            saldo =  saldo + 2
-                        j = j-1
-                    
-                        
-                        
-
-
-
-                                           
+                crypto_message = fernet.encrypt(message.encode('UTF-8'))
+                binary_message = Alg.string_to_binary(crypto_message)
+                message_6t, bal = algoritmo.encode_octets_to_patterns(binary_message)
                 #Saída dos processamentos
                 print(f"Mensagem enviada: {message}\n")
-                print(f"Binário da mensagem: {binary_message}\n")
+                print(f"Mensagem enviada criptografada: {crypto_message}\n")
+                print(f"Mensagem enviada em binário: {binary_message}\n")
                 print(f"Mensagem enviada em ternário: {message_6t}\n")
 
                 Alg.plot_signal(message_6t)
 
                 #Envio da mensagem
                 connection.send_message(message_6t.encode())
+
+            #-----------------------------------------SEM CRIPTOGRAFIA------------------------------------------------
+
+
+                # #Input de entrada
+                # message = input(f"Digite a mensagem para enviar ao servidor (ou 'exit' para sair):\n ")
+                # #Processamentos da mensagem
+                # binary_message = message
+
+                # message_6t, bal = algoritmo.encode_octets_to_patterns(binary_message)                
+                        
+                # #Saída dos processamentos
+                # print(f"Binário da mensagem: {binary_message}\n")
+                # print(f"Mensagem enviada em ternário: {bal}\n")
+
+                # Alg.plot_signal(bal)
+
+                # #Envio da mensagem
+                # connection.send_message(bal.encode())
 
 
         except KeyboardInterrupt:
@@ -113,6 +72,6 @@ def start_client(ip, port):
 if __name__ == "__main__":
     # Especifica o endereço IP e a porta do servidor
     IP = "192.168.100.74"
-    PORT = 8080
+    PORT = 8081
 
     start_client(IP, PORT)
--- Server/teste4.py
+++ Server/teste4.py
@@ -26,18 +26,63 @@ def generate_pattern_functions():
         if pattern in binary_map:
             return binary_map[pattern]
         else:
-            return "Binary string not found for input pattern."
+            # return "Binary string not found for input pattern."
+            return ""
+
 
     return get_pattern_from_binary, get_binary_from_pattern
 
-# # Generate the functions
-# pattern_to_pattern, pattern_to_binary = generate_pattern_functions()
+def DC_balance(octet_string):
+    dc_val=0
+    for i in range(0, len(octet_string), 1):
+        if octet_string[i]=='+':
+            dc_val+=1
+        elif octet_string[i]=='-':
+            dc_val-=1
+    return dc_val
 
-# # Example usage
-# binary_input = "000100010101001101010000"
-# result_pattern = pattern_to_pattern(binary_input)
-# print(f"The pattern corresponding to binary '{binary_input}' is '{result_pattern}'")
+def invert_pattern(pattern):
+    
+    stt1 = pattern.replace("+", "p")
+    stt2 = stt1.replace("-","m")
+    stt3 = stt2.replace("p","-")
+    stt4 = stt3.replace("m","+")
+    return stt4
 
-# pattern_input = "-0-0++-+-++0+--+0+"
-# result_binary = pattern_to_binary(pattern_input)
-# print(f"The binary corresponding to pattern '{pattern_input}' is '{result_binary}'")
+
+# Function to encode a long string of octets to patterns and concatenate them
+def encode_octets_to_patterns(long_octet_string):
+    pattern_to_pattern, _ = generate_pattern_functions()
+    
+    patterns = []
+    patterns_bal = []
+    dc_val=0
+    for i in range(0, len(long_octet_string), 8):  # Assuming octets are 8 bits each
+        octet = long_octet_string[i:i+8]
+        pattern = pattern_to_pattern(octet)
+        dc_val+=DC_balance(pattern)
+        if (dc_val >1 or dc_val<-1 ) and i >= 15:
+            inv_pattern = invert_pattern(pattern)
+            dc_val-=2*DC_balance(pattern)
+            patterns_bal.append(inv_pattern)
+        else:
+            patterns_bal.append(pattern)
+        patterns.append(pattern)
+    
+    return ''.join(patterns),''.join(patterns_bal)
+
+def decode_patterns_to_octets(long_ternary_string):
+    _,binary_map = generate_pattern_functions()
+    octets = []
+    for i in range(0, len(long_ternary_string), 6):  # Assuming ternary are 6 char each
+        ternary = long_ternary_string[i:i+6]
+        octet=binary_map(ternary)
+        if octet!="":
+            octets.append(octet)
+
+        else:
+            new_ternary=invert_pattern(ternary)
+            new_octet=binary_map(new_ternary)
+            octets.append(new_octet)
+        
+    return ''.join(octets)    
